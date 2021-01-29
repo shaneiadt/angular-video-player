ad75b30af0567102ee18829aa6feb07c4cf88fce
Shane O'Moore <shaneiadt@gmail.com>
2021-01-29 12:34:08 +0000

    emit playPause event from controls component

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-controls',
   templateUrl: './controls.component.html',
   styleUrls: ['./controls.component.scss']
 })
-export class ControlsComponent implements OnInit {
+export class ControlsComponent {
+  @Output() playPause = new EventEmitter();
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
+  video: HTMLVideoElement = document.createElement('video');
+  src: string = "https://pixabay.com/videos/download/video-31377_tiny.mp4?attachment"
+  isPlaying: Boolean = false;
+  isFullscreen: Boolean = false;
+  isMute: Boolean = false;
+  volume: number = 1;
+  playbackSpeed: number = 1;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.video = document.querySelector('video') as HTMLVideoElement;
+  }
+
+  onPlayPause() {
+    if (this.isPlaying) {
+      this.video.pause();
+    } else {
+      this.video.play();
+    }
+
+    this.isPlaying = !this.isPlaying;
   }
 
 }
